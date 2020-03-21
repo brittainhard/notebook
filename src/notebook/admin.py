5014a3fdd2977e6fa21486258b7b25bdda2b11ef
Brittain Hard <brittainhard@gmail.com>
2020-03-21 15:15:33 -0500

    Basic database functionality!

--- src/notebook/admin.py
+++ src/notebook/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Note, Tag
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Note)
--- src/notebook/models.py
+++ src/notebook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255, default="")
+    text = models.TextField(default="")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
