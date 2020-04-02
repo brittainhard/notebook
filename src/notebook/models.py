3030fde291789ebc9a60c89b2a9b5badf5deb1c4
Brittain Hard <brittainhard@gmail.com>
2020-04-02 15:16:53 -0500

    Adding in bootstrap.

--- src/notebook/models.py
+++ src/notebook/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, default="")
+    name = models.CharField(max_length=255, default="", unique=True)
 
     def __str__(self):
         return self.name
--- src/notebook/views.py
+++ src/notebook/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import Note
+from .models import Note, Tag
+
 
 class NoteView(generic.ListView):
+
     model = Note
     template_name = "notes.html"
     context_object_name = "notes"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
